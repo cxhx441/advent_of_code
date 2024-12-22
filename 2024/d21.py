9fb7b7960b342d95aa2215f03ca50dcf986bd997
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-12-22 01:05:32 -0600

    d21 failed

--- 2024/d21.py
+++ 2024/d21.py
@@ -1,154 +1,236 @@
 from timeit import default_timer as timer
 import collections
-import heapq
-from tabulate import tabulate
+
+class Pad:
+    def __init__(self):
+        self.pos = []
+        self.keys = [[]]
+        self.ROWS, self.COLS = len(self.keys), len(self.keys[0])
+        self.invalid_pos = [0, 0]
+        self.pressed_keys = []
+        self.key_to_coord = {}
+
+    def is_invalid_pos(self):
+        r, c = self.pos
+        if not ( 0 <= r < self.ROWS or 0 <= c < self.COLS) or self.pos == self.invalid_pos:
+            return True
+        return False
+
+    def _mv_right(self):
+        r, c = self.pos
+        self.pos[0], self.pos[1] = r, c + 1
+        if self.is_invalid_pos():
+            raise IndexError
+
+    def _mv_down(self):
+        r, c = self.pos
+        self.pos[0], self.pos[1] = r + 1, c
+        if self.is_invalid_pos():
+            raise IndexError
+
+    def _mv_left(self):
+        r, c = self.pos
+        self.pos[0], self.pos[1] = r, c - 1
+        if self.is_invalid_pos():
+            raise IndexError
+
+    def _mv_up(self):
+        r, c = self.pos
+        self.pos[0], self.pos[1] = r - 1, c
+        if self.is_invalid_pos():
+            raise IndexError
+
+    def send(self, ch):
+        if ch == '>':
+            self._mv_right()
+        elif ch == 'v':
+            self._mv_down()
+        elif ch == '<':
+            self._mv_left()
+        elif ch == '^':
+            self._mv_up()
+        elif ch == 'A':
+            self.press()
+        else:
+            raise ValueError
+
+    def press(self):
+        r, c = self.pos
+        pressed_key = self.keys[r][c]
+        self.pressed_keys.append(pressed_key)
+        return pressed_key
+
+
+class Keypad(Pad):
+    """
+    +---+---+---+
+    | 7 | 8 | 9 |
+    +---+---+---+
+    | 4 | 5 | 6 |
+    +---+---+---+
+    | 1 | 2 | 3 |
+    +---+---+---+
+        | 0 | A |
+        +---+---+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.pos = [3, 2]
+        self.keys = [ ['7', '8', '9'],
+                      ['4', '5', '6'],
+                      ['1', '2', '3'],
+             [float("inf"), '0', 'A']]
+        self.ROWS, self.COLS = len(self.keys), len(self.keys[0])
+        self.invalid_pos = [3, 0]
+        self.key_to_coord = {}
+        for r in range(self.ROWS):
+            for c in range(self.COLS):
+                if [r, c] == self.invalid_pos:
+                    continue
+                self.key_to_coord[self.keys[r][c]] = (r, c)
+
+
+class Dpad(Pad):
+    """
+        +---+---+
+        | ^ | A |
+    +---+---+---+
+    | < | v | > |
+    +---+---+---+
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.pos = [0, 2]
+        self.keys = [ [float("inf"), '^', 'A'],
+                      ['<', 'v', '>'] ]
+        self.ROWS, self.COLS = len(self.keys), len(self.keys[0])
+        self.invalid_pos = [0, 0]
+        self.key_to_coord = {}
+        for r in range(self.ROWS):
+            for c in range(self.COLS):
+                if [r, c] == self.invalid_pos:
+                    continue
+                self.key_to_coord[self.keys[r][c]] = (r, c)
+
 
 def parse_input(filename):
     with open(filename, 'r', encoding="UTF-8") as f:
-        racetrack = []
+        codes = []
         for line in f:
-            row = [ch for ch in line.strip()]
-            racetrack.append(row)
-    return racetrack
-
-def get_start(racetrack):
-    ROWS, COLS = len(racetrack), len(racetrack[0])
-    for r in range(ROWS):
-        for c in range(COLS):
-            if racetrack[r][c] == 'S':
-                return r, c
-
-def get_end(racetrack):
-    ROWS, COLS = len(racetrack), len(racetrack[0])
-    for r in range(ROWS):
-        for c in range(COLS):
-            if racetrack[r][c] == 'E':
-                return r, c
-
-def best_wo_cheats(racetrack):
-    start = get_start(racetrack)
-    q = collections.deque([(*start, 0, [start])])
-    seen = set()
-    while q:
-        r, c, ps, path = q.popleft()
-        if racetrack[r][c] == '#' or (r, c) in seen:
-            continue
-
-        seen.add((r, c))
-        if racetrack[r][c] == 'E':
-            return ps, path
-        for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            nr, nc = r + dr, c + dc
-            q.append( (nr, nc, ps + 1, path + [(nr, nc)] ) )
-
-def best_w_cheats(racetrack):
-    pass
+            codes.append(line.strip())
+    return codes
 
 
-def solution_1(racetrack, minimum_saving):
-    stnd = best_wo_cheats(racetrack)
-    ROWS, COLS = len(racetrack), len(racetrack[0])
-    counts = collections.defaultdict(lambda: 0)
-    for r in range(1, ROWS-1):
-        print(r)
-        for c in range(1, COLS-1):
-            if racetrack[r][c] == '#':
-                racetrack[r][c] = '.'
-                savings = stnd - best_wo_cheats(racetrack)
-                if savings >= minimum_saving:
-                    counts[savings] += 1
-                racetrack[r][c] = '#'
+def solution_1(codes):
+    def get_sequence(prev, target, dir_dic, pad):
+        if (prev, target) in dir_dic:
+            return dir_dic[(prev, target)]
 
-    return sum(counts.values())
-    # savings = find_best_with_cheats(racetrack, start, stnd)
-
-def dijkstra(grid, start):
-    ROWS, COLS = len(grid), len(grid[0])
-    dp = [ [float("inf")] * COLS for _ in range(ROWS) ]
-    h = [(0, *start)]
-    while h:
-        cost, r, c = heapq.heappop(h)
-        if cost >= dp[r][c] or grid[r][c] == '#':
-            continue
-        dp[r][c] = cost
-        for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            nr, nc = r + dr, c + dc
-            if not (0 <= nr < ROWS and 0 <= nc < COLS):
-                continue
-            heapq.heappush(h, (cost + 1, nr, nc))
-    return dp
-
-def get_radius_nodes(start, racetrack, radius):
-    deltas = set()
-    q = collections.deque([start])
-    for _ in range(radius):
-        r, c = q.popleft()
-        if (r, c) in deltas:
-            continue
-        deltas.add((r, c))
-        for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            nr, nc = r + dr, c + dc
-            q.append((nr, nc))
-    return deltas
-
-# def filter_deltas(racetrack, deltas, start):
-#     ROWS, COLS = len(racetrack), len(racetrack[0])
-#     filtered_deltas = set()
-#     for (r, c) in deltas:
-#         if not (0 <= r < ROWS and 0 <= c < COLS):
-#             continue
-
-
-
-def solution_2(racetrack, minimum_saving):
-    ROWS, COLS = len(racetrack), len(racetrack[0])
-    stnd, path = best_wo_cheats(racetrack)
-    end = get_end(racetrack)
-    dij_dp = dijkstra(racetrack, end)
-    # print(tabulate(dij_dp))
-
-    valid_cheats = collections.defaultdict(lambda: 0)
-    for standard_time, (r, c) in enumerate(path):
-
-        cheat_time = 0
-        q = collections.deque([(r, c, cheat_time)])
+        start = pad.key_to_coord[prev]
+        q = collections.deque( [ (*start, [] ) ] )
         seen = set()
         while q:
-            r, c, cheat_time = q.popleft()
-            if not ( 0 <= r < ROWS and 0 <= c < COLS ):
+            r, c, sequence = q.popleft()
+            if not ( 0 <= r < pad.ROWS or 0 <= c < pad.COLS) or [r, c] == pad.invalid_pos:
                 continue
-
-            if (r, c) in seen or cheat_time > 20:
+            if (r, c) in seen:
                 continue
             seen.add((r, c))
+            if (r, c) == pad.key_to_coord[target]:
+                pad.pos = list(pad.key_to_coord[prev])
+                sequence.sort()
+                try:
+                    for ch in sequence:
+                        pad.send(ch)
+                except IndexError:
+                    print('IndexError')
+                    sequence.sort(reverse=True)
+                # dir_dic[(prev, target)] = sequence.copy()
+                return sequence
 
-            if dij_dp[r][c] < float("inf"):
-                path_time = standard_time + dij_dp[r][c]
-                faster = cheat_time + path_time
-                saved = stnd - faster
-                if saved >= minimum_saving:
-                    valid_cheats[saved] += 1
-
-            for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            for dr, dc, dir in [
+                                (0, 1, '>'),
+                                (1, 0, 'v'),
+                                (0, -1, '<'),
+                                (-1, 0, '^')
+            ]:
                 nr, nc = r + dr, c + dc
-                q.append((nr, nc, cheat_time + 1))
-    return sum(valid_cheats.values())
+                q.append((nr, nc, sequence + [dir]))
+
+    keypad_dir = {}
+    dpad_dir = {}
+    prev_k = 'A'
+    prev_r1 = 'A'
+    prev_r0 = 'A'
+    def get_human_input(code):
+        """ hk -> r0 -> r1 """
+        nonlocal prev_k, prev_r1, prev_r0
+
+        print(code)
+        r1_sequence = []
+        for i, target in enumerate(code):
+            sub_seq = get_sequence(prev_k, target, keypad_dir, Keypad())
+            r1_sequence.extend(sub_seq)
+            r1_sequence.append('A')
+            prev_k = target
+        r1_sequence = ''.join(r1_sequence)
+        print(r1_sequence)
+
+        r0_sequence = []
+        for i, target in enumerate(r1_sequence):
+            sub_seq = get_sequence(prev_r1, target, dpad_dir, Dpad())
+            r0_sequence.extend(sub_seq)
+            r0_sequence.append('A')
+            prev_r1 = target
+        r0_sequence = ''.join(r0_sequence)
+        print(r0_sequence)
+
+        h_sequence = []
+        for target in r0_sequence:
+            sub_seq = get_sequence(prev_r0, target, dpad_dir, Dpad())
+            h_sequence.extend(sub_seq)
+            h_sequence.append('A')
+            prev_r0 = target
+        h_sequence = ''.join(h_sequence)
+        print(h_sequence)
+        print()
+        return h_sequence
+
+    h_inputs = []
+    for code in codes:
+        h_input = get_human_input(code)
+        h_inputs.append(h_input)
+
+    print()
+    for h in h_inputs:
+        print(h)
+
+    complexities = []
+    for code, h_input in zip(codes, h_inputs):
+        num = int( code.lstrip('0')[:-1] )
+        l = len(h_input)
+        complexities.append( num * l)
+
+    return sum(complexities)
+
+def solution_2():
+    return 0
 
 
 if __name__ == "__main__":
+    start = timer()
+    codes = parse_input("puzzle_input/d21_input.txt")
+    complexity = solution_1(codes)
+    end = timer()
+    print( f"{( end - start ) * 1000}ms" )
+    print(f'Solution1: {complexity}')
+
     # start = timer()
     # racetrack = parse_input("puzzle_input/d20_input.txt")
-    # result = solution_1(racetrack, 100)
+    # result = solution_2(racetrack, 100)
     # end = timer()
     # print( f"{( end - start ) * 1000}ms" )
     # print(f'Solution1: {result}')
-
-    start = timer()
-    # racetrack = parse_input("puzzle_input/d20_input_ex1.txt")
-    # result = solution_2(racetrack, 50)
-    racetrack = parse_input("puzzle_input/d20_input.txt")
-    result = solution_2(racetrack, 100)
-    end = timer()
-    print( f"{( end - start ) * 1000}ms" )
-    print(f'Solution1: {result}')
 
--- 2024/d21_test.py
+++ 2024/d21_test.py
@@ -3,26 +3,39 @@ import unittest
 from d21 import *
 
 class SolutionTest(unittest.TestCase):
-    # def test_input_parsing_on_example1(self):
-    #     available, desired = parse_input("puzzle_input/d20_input_ex1.txt")
-    #     self.assertEqual(['r', 'wr', 'b', 'g', 'bwu', 'rb', 'gb', 'br'], available)
-    #     self.assertEqual(['brwrr', 'bggr', 'gbbr', 'rrbgbr', 'ubwu', 'bwurrg', 'brgr', 'bbrgwb'], desired)
+    def test_input_parsing_on_example1(self):
+        codes = parse_input("puzzle_input/d21_input_ex1.txt")
+        self.assertEqual(codes[0], '029A')
+        self.assertEqual(codes[4], '379A')
+
+
+    def test_can_input_to_get_correct_codes(self):
+        A_h = '<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A'
+        A_r0 = 'v<<A>>^A<A>AvA<^AA>A<vAAA>^A'
+        A_r1 = '<A^A>^^AvvvA'
+        A_k = '029A'
+
+        dpad_R0 = Dpad()
+        dpad_R1 = Dpad()
+        keypad = Keypad()
+
+
+        for i, ch in enumerate(A_h):
+            dpad_R0.send(ch)
+        self.assertEqual( ''.join(dpad_R0.pressed_keys), A_r0 )
+
+        for ch in dpad_R0.pressed_keys:
+            dpad_R1.send(ch)
+        self.assertEqual( ''.join(dpad_R1.pressed_keys), A_r1 )
+
+        for ch in dpad_R1.pressed_keys:
+            keypad.send(ch)
+        self.assertEqual( ''.join(keypad.pressed_keys), A_k )
 
     def test_solution1_parsing_on_example1(self):
-        racetrack = parse_input("puzzle_input/d20_input_ex1.txt")
-        stnd = best_wo_cheats(racetrack)
-        self.assertEqual(84, stnd)
-
-        total = solution_1(racetrack, 1)
-        supposed2be = 14 + 14 + 2 + 4 + 2 + 3 + 1 + 1 + 1 + 1 + 1
-        self.assertEqual(supposed2be, total)
-
-    def test_solution2_parsing_on_example1(self):
-        racetrack = parse_input("puzzle_input/d20_input_ex1.txt")
-        total = solution_2(racetrack, 50)
-        supposed2be = sum([32, 31, 29, 39, 25, 23, 20, 19, 12, 14, 12, 22, 4, 3])
-        print(supposed2be, total)
-        self.assertEqual(supposed2be, total)
+        codes = parse_input("puzzle_input/d21_input_ex1.txt")
+        complexity = solution_1(codes)
+        self.assertEqual(126384, complexity)
 
 if __name__ == '__main__':
     unittest.main()
