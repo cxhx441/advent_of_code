4550fe6398e2ce8adbf39cfe1e9f8c1e29f54b26
Craig H <70126287+cxhx441@users.noreply.github.com>
2025-12-08 09:17:04 -0800

    ignore file wo extension

--- 2025/tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-    // printf("%d,%d,%d\n", junction_boxes[last_a].x, junction_boxes[last_a].y, junction_boxes[last_a].z);
-    // printf("%d,%d,%d\n", junction_boxes[last_b].x, junction_boxes[last_b].y, junction_boxes[last_b].z);
-    // printf("%d\n", junction_boxes[last_a].x * junction_boxes[last_b].x);
