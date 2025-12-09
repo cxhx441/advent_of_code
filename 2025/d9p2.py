cc468daafb1c6409ad711d90e65b3681a891b997
Craig H <70126287+cxhx441@users.noreply.github.com>
2025-12-09 15:06:48 -0800

    sigh

--- 2025/d9p2.py
+++ 2025/d9p2.py
@@ -5,10 +5,12 @@ class Coordinate():
         self.x = int(x)
         self.y = int(y)
 
-redgreen = set()
-def set_redgreen():
+vlines = set()
+hlines = set()
+green = set()
+def set_green():
     for coord in coords:
-        redgreen.add((coord.x, coord.y))
+        green.add((coord.x, coord.y))
 
     for i in range(1, len(coords)):
         c0 = coords[i-1]
@@ -18,7 +20,8 @@ def set_redgreen():
                 c0, c1 = c1, c0
             y0, y1 = c0.y, c1.y
             while y0 < y1:
-                redgreen.add((c0.x, y0))
+                green.add((c0.x, y0))
+                vlines.add((c0.x, y0))
                 y0 += 1
 
         elif c0.y == c1.y:
@@ -27,7 +30,8 @@ def set_redgreen():
 
             x0, x1 = c0.x, c1.x
             while x0 < x1:
-                redgreen.add((x0, c0.y))
+                green.add((x0, c0.y))
+                hlines.add((x0, c0.y))
                 x0 += 1
 
     c0 = coords[0]
@@ -37,7 +41,8 @@ def set_redgreen():
             c0, c1 = c1, c0
         y0, y1 = c0.y, c1.y
         while y0 < y1:
-            redgreen.add((c0.x, y0))
+            green.add((c0.x, y0))
+            vlines.add((c0.x, y0))
             y0 += 1
 
     elif c0.y == c1.y:
@@ -46,7 +51,8 @@ def set_redgreen():
 
         x0, x1 = c0.x, c1.x
         while x0 < x1:
-            redgreen.add((x0, c0.y))
+            green.add((x0, c0.y))
+            hlines.add((x0, c0.y))
             x0 += 1
 
 # def get_known_inner():
@@ -64,7 +70,7 @@ def set_redgreen():
 # 269, 2456 >
 
 fname = "2025/puzzle_input/d9p1_input.txt"
-# fname = "2025/puzzle_input/d9p1_example.txt"
+fname = "2025/puzzle_input/d9p1_example.txt"
 coords = []
 # def read_input(filename):
 maxx = maxy = 0
@@ -78,63 +84,188 @@ with open(fname, 'r', encoding="UTF-8") as f:
         minx = min(minx, int(x))
         miny = min(miny, int(y))
 
-set_redgreen()
-redgreenog = redgreen.copy()
-known_green_inner = (8, 2)
+# print("lennnn")
+# print(len(coords))
+# print(len(set([ (coord.x, coord.y) for coord in coords])))
+coordset = set([ (coord.x, coord.y) for coord in coords])
+
+set_green()
+# redgreenog = redgreen.copy()
+# known_green_inner = (8, 2)
 # known_green_inner = (98282,51319)
 
-def fill_redgreen(known_green_inner):
-    q = [known_green_inner]
-    nei = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    # q = None
-    while q:
-        lq = len(q)
-        for _ in range(lq):
-            x, y = q.pop()
-            if (x, y) in redgreen:
-                continue
-            redgreen.add((x, y))
-            for dx, dy in nei:
-                nx, ny = x + dx, y + dy
-                if (0 <= nx <= maxx and 0 <= ny <= maxy):
-                    q.append((nx, ny))
+# def fill_redgreen(known_green_inner):
+#     q = [known_green_inner]
+#     nei = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+#     # q = None
+#     while q:
+#         lq = len(q)
+#         for _ in range(lq):
+#             x, y = q.pop()
+#             if (x, y) in redgreen:
+#                 continue
+#             redgreen.add((x, y))
+#             for dx, dy in nei:
+#                 nx, ny = x + dx, y + dy
+#                 if (0 <= nx <= maxx and 0 <= ny <= maxy):
+#                     q.append((nx, ny))
 
-# for j in range(miny-2, maxy + 2):
-#     line = []
-#     for i in range(minx -2, maxx + 2):
-#         if (i, j) in redgreen:
-#             line.append('X')
-#         else:
-#             line.append('.')
-#     print(''.join(line))
+for coord in coordset:
+    if coord in vlines: vlines.remove(coord)
+    if coord in hlines: hlines.remove(coord)
 
-seen = set()
-for coord in coords:
-    nei = [(0, 1), (1, 0), (-1, 0), (-1, 0)]
-    x, y = coord.x, coord.y
-    for dx, dy in nei:
-        redgreen = redgreenog.copy()
-        print("filling")
-        fill_redgreen((x + dx, y + dy))
-        print("filled")
-        largest_area = 0
-        n = len(coords)
-        for i in range(n):
-            for j in range(i + 1, n):
-                x1 = coords[i].x
-                y1 = coords[i].y
-                x2 = coords[j].x
-                y2 = coords[j].y
-                h = abs(y2 - y1) + 1
-                w = abs(x2 - x1) + 1
-                area = w * h
-                # area is larger && both mirrored points are either red or between two red points
-                if (area > largest_area):
-                    mirrored1 = Coordinate(x2, y1)
-                    mirrored2 = Coordinate(x1, y2)
+for j in range(miny-2, maxy + 2):
+    line = []
+    for i in range(minx -2, maxx + 2):
+        if (i, j) in coordset:
+            line.append('X')
+        elif (i, j) in green:
+            line.append('#')
+        else:
+            line.append('.')
+    print(''.join(line))
 
-                    if (x1, y2) in redgreen and (x2, y1) in redgreen:
-                        largest_area = area
-        seen.add(largest_area)         # printf("%lu: %d, %d ,, %d, %d\n", largest_area, x1, y1, x2, y2);
-        print(largest_area)
-print(seen)
+print("v")
+for j in range(miny-2, maxy + 2):
+    line = []
+    for i in range(minx -2, maxx + 2):
+        if (i, j) in coordset and (i, j) in vlines:
+            line.append('X')
+        elif (i, j) in green and (i, j) in vlines:
+            line.append('#')
+        else:
+            line.append('.')
+    print(''.join(line))
+
+print("h")
+for j in range(miny-2, maxy + 2):
+    line = []
+    for i in range(minx -2, maxx + 2):
+        if (i, j) in coordset and (i, j) in hlines:
+            line.append('X')
+        elif (i, j) in green and (i, j) in hlines:
+            line.append('#')
+        else:
+            line.append('.')
+    print(''.join(line))
+
+# seen = set()
+# for coord in coords:
+    # nei = [(0, 1), (1, 0), (-1, 0), (-1, 0)]
+    # x, y = coord.x, coord.y
+    # for dx, dy in nei:
+    # redgreen = redgreenog.copy()
+    # print("filling")
+    # # fill_redgreen((x + dx, y + dy))
+    # print("filled")
+def check_rectangle(c0, c1):
+    ''' y |
+        x --
+
+        c0 . . .c0m
+        .       .
+        .       .
+        c1m. . . c1
+    '''
+
+    def vedgecount(c):
+        count1 = 0
+        for x in range(c.x):
+            if (x, c.y) in vlines:
+                count1 += 1
+        count2 = 0
+        for x in range(maxx+10, c.x, -1):
+            if (x, c.y) in vlines:
+                count2 += 1
+        return count1 % 2 == 1 and count2 % 2 == 1
+    def hedgecount(c):
+        count1 = 0
+        for y in range(c.y):
+            if (c.x, y) in hlines:
+                count1 += 1
+
+        count2 = 0
+        for y in range(maxy+10, c.y, -1):
+            if (c.x, y) in hlines:
+                count2 += 1
+
+        return count1 % 2 == 1 and count2 % 2 == 1
+
+
+    if (c0.x**2 + c0.y**2) > (c1.x**2 + c1.y**2):
+        c0, c1 = c1, c0
+
+    c0m = Coordinate(c1.x, c0.y)
+    c1m = Coordinate(c0.x, c1.y)
+
+    if not ( (c0m.x, c0m.y) in green | coordset or vedgecount(c0m) and hedgecount(c0m) ) :
+        return False
+    if not ( (c1m.x, c1m.y) in green | coordset or vedgecount(c1m) and hedgecount(c1m) ) :
+        return False
+
+    print("")
+    for j in range(miny-2, maxy + 2):
+        line = []
+        for i in range(minx -2, maxx + 2):
+            if (i, j) == (c0.x, c0.y):
+                line.append('1')
+            elif (i, j) == (c0m.x, c0m.y):
+                line.append('2')
+            elif (i, j) == (c1.x, c1.y):
+                line.append('3')
+            elif (i, j) == (c1m.x, c1m.y):
+                line.append('4')
+            elif (i, j) in coordset:
+                line.append('X')
+            elif (i, j) in green:
+                line.append('#')
+            else:
+                line.append('.')
+        print(''.join(line))
+
+    # for x in range(c0.x, c0m.x + 1): # c0 -> c0m
+    #     if (x, c0.y) in vlines:
+    #         print("fail")
+    #         return False
+    # for y in range(c0.y, c1m.y + 1): # c0 -> c1m
+    #     if (c0.x, y) in hlines:
+    #         print("fail")
+    #         return False
+    # for x in range(c1m.x, c1.x + 1): # c1m -> c1
+    #     if (x, c1m.y) in vlines:
+    #         print("fail")
+    #         return False
+    # for y in range(c0m.y, c1.y + 1): # c0m -> c1
+    #     if (c0m.x, y) in hlines:
+    #         print("fail")
+    #         return False
+
+    print("succeed")
+    return True
+
+
+
+
+largest_area = 0
+n = len(coords)
+for i in range(n):
+    for j in range(i + 1, n):
+        x1 = coords[i].x
+        y1 = coords[i].y
+        x2 = coords[j].x
+        y2 = coords[j].y
+        h = abs(y2 - y1) + 1
+        w = abs(x2 - x1) + 1
+        area = w * h
+        # area is larger && both mirrored points are either red or between two red points
+        if (area > largest_area):
+            if check_rectangle(coords[i], coords[j]):
+                largest_area = area
+            # mirrored1 = Coordinate(x2, y1)
+            # mirrored2 = Coordinate(x1, y2)
+
+            # if (x1, y2) in redgreen and (x2, y1) in redgreen:
+            #     largest_area = area
+# seen.add(largest_area)         # printf("%lu: %d, %d ,, %d, %d\n", largest_area, x1, y1, x2, y2);
+print(largest_area)
+# print(seen)
